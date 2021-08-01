9b8fc241954421b8cb1a346fc243382d0b5499da
Tiago Seabra <tlgs@users.noreply.github.com>
2021-08-01 01:14:20 +0100

    feat: add 0830

---
+++ 0830.py
@@ -0,0 +1,13 @@
+"""830. Positions of Large Groups"""
+
+
+class Solution:
+    def largeGroupPositions(self, s: str) -> List[List[int]]:
+        idx, res = 0, []
+        for _, g in itertools.groupby(s):
+            x = len(list(g))
+            if x >= 3:
+                res.append([idx, idx + x - 1])
+            idx += x
+
+        return res
