a8b6b1c728a3373940b048303a69765a03160a62
Tiago Seabra <tlgs@users.noreply.github.com>
2021-08-01 03:28:53 +0100

    feat: add 1894

---
+++ 1894.py
@@ -0,0 +1,12 @@
+"""1894. Find the Student that Will Replace the Chalk"""
+
+
+class Solution:
+    def chalkReplacer(self, chalk: List[int], k: int) -> int:
+        k = k % sum(chalk)
+
+        i = 0
+        while (k := k - chalk[i]) >= 0:
+            i += 1
+
+        return i
