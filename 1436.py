70487ccd37e5ee57740bc757071cad98f800f4de
Tiago Seabra <tlgs@users.noreply.github.com>
2021-08-01 01:11:36 +0100

    feat: add 1436

---
+++ 1436.py
@@ -0,0 +1,11 @@
+"""1436. Destination City"""
+
+
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        a, b = set(), set()
+        for (x, y) in paths:
+            a.add(x)
+            b.add(y)
+
+        return (b - a).pop()
