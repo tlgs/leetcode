52e733c7fffbad12516259f43c200905d27ce231
Tiago Seabra <tlgs@users.noreply.github.com>
2021-08-02 01:53:00 +0100

    feat: add 0654

---
+++ 0654.py
@@ -0,0 +1,26 @@
+"""654. Maximum Binary Tree"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def constructMaximumBinaryTree(self, nums: List[int]) -> TreeNode:
+        if not nums:
+            return None
+
+        stack = []
+        for v in nums:
+            node = TreeNode(v, None, None)
+
+            while stack and stack[-1].val < v:
+                node.left = stack.pop()
+
+            if stack:
+                stack[-1].right = node
+
+            stack.append(node)
+
+        return stack[0]
