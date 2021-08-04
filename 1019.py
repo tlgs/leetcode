922e828cfba5b4ebc043fde74107ad31f116e733
Tiago Seabra <tlgs@users.noreply.github.com>
2021-08-04 17:53:31 +0100

    feat: add 1019

---
+++ 1019.py
@@ -0,0 +1,24 @@
+"""1019. Next Greater Node In Linked List"""
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def nextLargerNodes(self, head: ListNode) -> List[int]:
+        nums = []
+        while head:
+            nums.append(head.val)
+            head = head.next
+
+        ans = [0] * len(nums)
+        stack = []
+        for i, v in enumerate(nums):
+            while stack and nums[stack[-1]] < v:
+                ans[stack.pop()] = v
+
+            stack.append(i)
+
+        return ans
