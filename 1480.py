ea5e056b6936a4ae737c88cc020c7abf6a6c4209
Tiago Seabra <tlgs@users.noreply.github.com>
2021-08-11 02:42:12 +0100

    feat: add 1480

---
+++ 1480.py
@@ -0,0 +1,13 @@
+"""1480. Running Sum of 1d Array (EASY)
+
+tags: array
+"""
+
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        cumsum = nums[:1]
+        for v in nums[1:]:
+            cumsum.append(v + cumsum[-1])
+
+        return cumsum
