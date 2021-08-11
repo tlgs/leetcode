7b281eec4df23c2c87b12fa96708cc5300deaaa9
Tiago Seabra <tlgs@users.noreply.github.com>
2021-08-12 00:23:03 +0100

    feat: add 0894

---
+++ 0894.py
@@ -0,0 +1,29 @@
+"""894. All Possible Full Binary Trees
+
+difficulty: medium
+tags: dynamic programming, binary tree
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    mem = {1: [TreeNode(0)]}
+
+    def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
+        if n % 2 == 0:
+            return []
+
+        if n not in self.mem:
+            self.mem[n] = [
+                TreeNode(0, left, right)
+                for i in range(1, n, 2)
+                for left in self.allPossibleFBT(i)
+                for right in self.allPossibleFBT(n - 1 - i)
+            ]
+
+        return self.mem[n]
