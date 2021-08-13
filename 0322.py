aeb8f34cf07867801c3295b7d41ace30ff92d64a
Tiago Seabra <tlgs@users.noreply.github.com>
2021-08-13 01:11:53 +0100

    feat: add 0322

---
+++ 0322.py
@@ -0,0 +1,20 @@
+"""322. Coin Change
+
+difficulty: medium
+tags: dynamic programming
+"""
+import sys
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        coins.sort()
+
+        dp = [0] + [sys.maxsize for _ in range(amount)]
+        for value in range(1, amount + 1):
+            for coin in coins:
+                if value >= coin:
+                    if dp[value - coin] + 1 < dp[value]:
+                        dp[value] = dp[value - coin] + 1
+
+        return dp[-1] if dp[-1] != sys.maxsize else -1
